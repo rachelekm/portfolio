40b0d5d47c1fe98a7e838c7f2f4be36941b8dcf6
Rachele Morino <rachelekm@gmail.com>
2021-01-01 17:52:58 -0500

    nav element responsive design

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 const clientView = {
     onPageLoad: function(){
+        $('.navList').on('click', e => {
+            e.preventDefault();
+            e.stopPropagation();
+            alert('clicked@');
+        });
     }
 }
 
